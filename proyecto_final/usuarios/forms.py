6eb5368048bbf8db7cad444fd8ae4bda9d200a36
NicolasKowal <nicolas.kowal95@gmail.com>
2023-06-20 15:29:15 -0300

    sincronizar

--- proyecto_final/usuarios/forms.py
+++ proyecto_final/usuarios/forms.py
@@ -19,9 +19,11 @@ class UserEditForm(UserRegisterForm):
     email = forms.EmailField(label="Ingrese su email:")
     password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repetir la contraseña', widget=forms.PasswordInput)
-    last_name = forms.CharField()
-    first_name = forms.CharField()
+    first_name = forms.CharField(label='Nombre')
+    last_name = forms.CharField(label='Apellido')
+
 
     class Meta:
         model = User
-        fields = ['email', 'password1', 'password2', 'last_name', 'first_name']
+        fields = ['email', 'password1', 'password2', 'first_name', 'last_name' ]
+        exclude = ['username']
