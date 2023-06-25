cb3d46841897f78efeb07cb9678293ff7c8fefc9
NicolasKowal <nicolas.kowal95@gmail.com>
2023-06-25 16:14:44 -0300

    .

--- proyecto_final/usuarios/urls.py
+++ proyecto_final/usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from usuarios import views, viewscrud
 from django.contrib.auth.views import LogoutView
+# from mascotas import views
 
 urlpatterns = [
     path('login', views.login_request, name="Login"),
@@ -11,17 +12,13 @@ urlpatterns = [
     path('Pagina_principal', views.main, name ='main'),
     path('Editar_perfil', views.editarPerfil, name='editarPerfil'),
     path('Editar_contrasena', views.editarPass, name='editarContrasena'),
-    
     path('cambiar-avatar', views.cambiarAvatar, name='cambiar_avatar'),
-
-    
-    
 ]
 
-urlpatterns += [
-    path('d', viewscrud.User_List.as_view(), name='List'),
-    path('<int:pk>', viewscrud.User_Detail.as_view(), name='Detail'),
-    path('nuevo', viewscrud.User_View.as_view(), name='New'),
-    path('editar/<int:pk>', viewscrud.User_Update.as_view(), name='Edit'),
-    path('borrar/<int:pk>', viewscrud.User_Delete.as_view(), name='Delete'),
-]
+# urlpatterns += [
+#     path('d', viewscrud.User_List.as_view(), name='List'),
+#     path('<int:pk>', viewscrud.User_Detail.as_view(), name='Detail'),
+#     path('nuevo', viewscrud.User_View.as_view(), name='New'),
+#     path('editar/<int:pk>', viewscrud.User_Update.as_view(), name='Edit'),
+#     path('borrar/<int:pk>', viewscrud.User_Delete.as_view(), name='Delete'),
+# ]
