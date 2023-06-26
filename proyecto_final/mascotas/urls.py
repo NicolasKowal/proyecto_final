221efc6c482692ee7160b55cfa0f2feb08a5fa67
Virginia Bottino <virginia.bottino@gmail.com>
2023-06-25 22:48:31 -0300

    Vistas con loginrequiered redirigen a login

--- proyecto_final/mascotas/urls.py
+++ proyecto_final/mascotas/urls.py
@@ -13,10 +13,4 @@ urlpatterns = [
     path('lista-perros/', views.PerroList.as_view(), name="perros"),
     path('lista-gatos/', views.GatoList.as_view(), name="gatos"),
     path('lista-otros/', views.OtroList.as_view(), name="otros"),
-    # path('editarPerro/<pk>/', views.PerroUpdate.as_view(), name="editar perro"),
-    # path('editarGato/<pk>/', views.GatoUpdate.as_view(), name="editar gato"),
-    # path('editarOtro/<pk>/', views.OtroUpdate.as_view(), name="editar otro"),
-    # path('eliminarPerro/<pk>/', views.PerroDelete.as_view(), name="eliminar perro"),
-    # path('eliminarGato/<pk>/', views.GatoDelete.as_view(), name="eliminar gato"),
-    # path('eliminarOtro/<pk>/', views.OtroDelete.as_view(), name="eliminar otro"),
 ]
